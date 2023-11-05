eb453c1cc5c482da19855b8cc30f7c698fa43b17
Juan Corzo <juancorzo@hotmail.com>
2023-11-05 17:33:39 -0500

    Added the AddBooks Layout activity_add_books.xml and AddBooks.java the latter is not implemented

---
+++ app/src/main/java/com/example/library_management_project/AddBooks.java
@@ -0,0 +1,14 @@
+package com.example.library_management_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddBooks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_books);
+    }
+}
