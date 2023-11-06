18d41c6eb1bca6b0980b522c2c6fdbc3bba6fdf9
Diego Corzo <dc29096@gmail.com>
2023-11-05 21:10:40 -0500

    Search by ISBN, Author, and title added.

--- app/src/main/java/com/example/library_management_project/Search.java
+++ app/src/main/java/com/example/library_management_project/Search.java
@@ -1,46 +1,116 @@
 package com.example.library_management_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import model.Book;
 
 
 public class Search extends AppCompatActivity {
+    private Spinner searchSpinner;
+    private Button searchButton;
+    private ListView resultListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Spinner spn;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
         spn = findViewById(R.id.spinner);
+
+        resultListView = findViewById(R.id.displayBooksListView);
+
+        searchSpinner = findViewById(R.id.spinner);
+        searchButton = findViewById(R.id.searchBooksButton);
 
         ArrayList<String> searchType = new ArrayList<String>();
         searchType.add("IBM");
         searchType.add("Title");
         searchType.add("Author");
-        searchType.add("Year Released");
+        ArrayAdapter<String> adapterSet = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, searchType);
+        spn.setAdapter(adapterSet);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,searchType);
-        spn.setAdapter(adapter);
-       /*
-        spn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String item = adapterView.getItemAtPosition(i).toString();
-                Toast.makeText(SpinnerTest.this, item, Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+
+                checkBook();
+            }
+        });
+    }
+    public void checkBook() {
+        int index = searchSpinner.getSelectedItemPosition();
+        String path = "";
+        switch(index){
+            case 0:
+                path = "isbn";
+                break;
+            case 1:
+                path = "title";
+                break;
+            case 2:
+                path = "author";
+                break;
+        }
+
+        EditText search = findViewById(R.id.searchEditText);
+        String searchBy = search.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Book");
+        Query checkUserData = reference.orderByChild(path).equalTo(searchBy);
+        checkUserData.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ArrayList<Book> bookList = new ArrayList<>();
+                ArrayAdapter bookAdapter = new ArrayAdapter<model.Book>(Search.this, android.R.layout.simple_list_item_1, bookList);
+                resultListView.setAdapter(bookAdapter);
+                if (snapshot.exists()) {
+
+                    for (DataSnapshot bookSnapshot : snapshot.getChildren()) {
+                        String isbn = bookSnapshot.child("isbn").getValue(String.class);
+                        String author = bookSnapshot.child("author").getValue(String.class);
+                        String title = bookSnapshot.child("title").getValue(String.class);
+
+                        Calendar calendar = Calendar.getInstance();
+                        Date today = calendar.getTime();
+
+                        model.Book book = new model.Book(isbn, author, title, today);
+                        bookList.add(book);
+                    }
+                }
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
+            public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
-        */
     }
 }
